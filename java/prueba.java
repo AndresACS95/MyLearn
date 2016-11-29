870be8d5f2f86638127d103625c6d2b776dc3208
e3r8ick <eguicoro2@gmail.com>
2016-11-29 08:56:17 -0600

    Add files via upload
    Codifo papa enctriptación de java sha256

--- java/prueba.java
+++ java/prueba.java
@@ -1,12 +1,37 @@
-public class MethodInvocation extends Applet {
-      public void noArgMethod() { ... }
-      public void someMethod(String arg) { ... }
-      public void someMethod(int arg) { ... }
-      public int  methodReturningInt() { return 5; }
-      public String methodReturningString() { return "Hello"; }
-      public OtherClass methodReturningObject() { return new OtherClass(); }
-  }
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encriptacion;
 
-public class OtherClass {
-  public void anotherMethod();
+/**
+ *
+ * @author e3r8ick
+ */
+public class Encriptacion {
+
+     /* Retorna un hash a partir de un tipo y un texto */
+    public static String getHash(String txt, String hashType) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest
+                    .getInstance(hashType);
+            byte[] array = md.digest(txt.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100)
+                        .substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /* Retorna un hash SHA1 a partir de un texto */
+    public static String sha256(String txt) {
+        return Encriptacion.getHash(txt, "SHA-256");
+    }
+
 }
